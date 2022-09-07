f9c42d2a21708f43133df2512f835e26fa51acae
tima620 <snowyandroid@gmail.com>
2022-09-08 00:06:17 +0900

    まずは入れ物だけ作成する

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { createRoot } from 'react-dom/client'
+import Split from 'react-split'
+import ReactFlow, { Background, Controls } from 'react-flow-renderer'
+
+type Query = string
+
+// SQLを入力するフォーム
+const SQLForm = () => {
+  const [query, setQuery] = useState<Query>('select *\nfrom table1')
+
+  return (
+    <form style={{ border: '1px solid black', marginBlockEnd: '0em', background: 'lightgray' }}>
+      <label>
+        SQL: <br />
+        <textarea
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          style={{ width: '100%' }}
+          rows={30}
+        />
+      </label>
+      <input type='button' value='create graph' />
+    </form>
+  )
+}
+
+// グラフ
+const Graph = () => {
+  return (
+    <ReactFlow style={{ border: '1px solid black' }}>
+      <Controls />
+      <Background />
+    </ReactFlow>
+  )
+}
+
+// 全体
+const App = () => {
+  return (
+    <Split sizes={[30, 70]} style={{ display: 'flex', height: '500px' }}>
+      <SQLForm />
+      <Graph />
+    </Split>
+  )
+}
 
 const container = document.getElementById('app')!
 const root = createRoot(container)
-root.render(<div>hello</div>)
+root.render(<App />)
